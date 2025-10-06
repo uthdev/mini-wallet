93c092e374bf4517b19ce693186174bde8bf34e2
uthdev <adelekegbolahan92@yahoo.com>
2025-10-06 07:51:17 +0100

    fix: add validation decorators to CreateTransactionInput

--- src/transactions/dto/create-transaction.input.ts
+++ src/transactions/dto/create-transaction.input.ts
@@ -1,13 +1,18 @@
 import { Field, Float, InputType } from '@nestjs/graphql';
+import { IsUUID, IsString, IsNumber, Min } from 'class-validator';
 
 @InputType()
 export class CreateTransactionInput {
   @Field()
+  @IsUUID()
   walletId: string;
 
   @Field()
+  @IsString()
   toAddress: string;
 
   @Field(() => Float)
+  @IsNumber()
+  @Min(0.00000001)
   amountBtc: number;
 }
